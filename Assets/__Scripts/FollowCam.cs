a69da199bda469e29e6495328f3a339b675754c7
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-02-13 17:16:20 -0500

    Castle, Projectile Trail, and Goal
    Added the game objects/scripts for the castle, projectile trail, and
    goal.

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -19,11 +19,24 @@ public class FollowCam : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		if (poi == null)
-			return; // return if there is no poi
-
-		// Get the position of the poi
-		Vector3 destination = poi.transform.position;
+		Vector3 destination;
+		// If there is no poi, return to P:[0,0,0]
+		if (poi == null) {
+			destination = Vector3.zero;
+		} else {
+			// Get the position of the poi
+			destination = poi.transform.position;
+			// If poi is a Projectile, check to see if it's at rest
+			if (poi.tag == "Projectile"){
+				// if it is sleeping (that is, not moving)
+				if (poi.rigidbody.IsSleeping()){
+					// return to default view
+					poi = null;
+					// in the next update
+					return;
+				}
+			}
+		}
 		// Limit the X & Y to minimum values
 		destination.x = Mathf.Max (minXY.x, destination.x);
 		destination.y = Mathf.Max (minXY.y, destination.y);
@@ -33,6 +46,7 @@ public class FollowCam : MonoBehaviour {
 		destination.z = camZ;
 		// Set the camera to the destination
 		transform.position = destination;
+
 		// Set the orthographicSize of the Camera to keep Ground in view
 		this.camera.orthographicSize = destination.y + 10;
 	}
--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	static public Slingshot S;
 	// Fields set in the Unity Inspector pane
 	public GameObject prefabProjectile;
 	public float velocityMult = 4f;
@@ -13,6 +14,9 @@ public class Slingshot : MonoBehaviour {
 	public bool aimingMode;
 
 	void Awake(){
+		// Set the Slingshot singleton S
+		S = this;
+
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchpoint = launchPointTrans.gameObject;
 		launchpoint.SetActive(false);
